735f433beeba0283a53010dbc013010326b547ec
Ashton Creager <20607@my4county.net>
2021-09-16 10:51:28 -0400

    complete

---
+++ repos/VariableDemo/src/variabledemo/VariableDemo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variabledemo;
+
+import java.util.Scanner;
+import java.util.*;
+/**
+ *
+ * @author acreager
+ */
+public class VariableDemo {
+
+    /**
+     * char
+     * boolean
+     * int
+     * double
+     * string
+     */
+    public static void main(String[] args) {
+        
+        char initial = 'M';
+        System.out.println(initial);
+        System.out.println("");
+        
+    }
+          
+    
+}
